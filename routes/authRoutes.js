63a3d78291482e4666d5c9792cd68fc71f667748
irfanwalele <irfanwalele@gmail.com>
2019-09-18 21:29:58 -0400

    commented out routes-->authRoutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const userController = require('../controllers/userController')
 
-// /auth
-router.route("/login")
-   // .all(passport.authenticate('local'))
-    .post(userController.login)
+// // /auth
+// router.route("/login")
+//    // .all(passport.authenticate('local'))
+//     .post(userController.login)
 
-router.post("/login", passport.authenticate('local'), (req, res) => {
-    userController.login(req, res);
-})
+// router.post("/login", passport.authenticate('local'), (req, res) => {
+//     userController.login(req, res);
+// })
 
 module.exports = router;
